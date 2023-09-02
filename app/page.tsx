952b8b9600c0263867cf9596e270e16d009a066b
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-09-01 18:17:02 -0600

    correcciones

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,57 @@ export default async function IndexPage() {
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex flex-col items-center gap-2">
-        <p className="text-2xl font-bold"> Housing Prices </p>
-        <Imagen1 />
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+        <div className="flex">
+          {/* Mitad Izquierda: Texto */}
+          <div className="w-1/2 p-6">
+            <h2 className="text-2xl font-bold">Título de la sección</h2>
+            <p className="mt-4">Aquí puedes agregar tu contenido de texto.</p>
+          </div>
+
+          {/* Mitad Derecha: Imagen */}
+          <div className="w-1/2">
+            <Imagen1 />
+          </div>
+        </div>
+      </section>
+      <section className="grid grid-cols-2 grid-rows-2 gap-4">
+      {/* Recuadro 1 */}
+      <div className="p-4 border border-gray-300 rounded-lg">
+        <div className="flex items-center mb-2">
+          <div className="w-8 h-8 bg-blue-500 rounded-full"></div> {/* Reemplaza con tu icono */}
+          <h3 className="ml-2 text-lg font-bold">Título 1</h3>
+        </div>
+        <p>Texto descriptivo para el Recuadro 1.</p>
       </div>
+
+      {/* Recuadro 2 */}
+      <div className="p-4 border border-gray-300 rounded-lg">
+        <div className="flex items-center mb-2">
+          <div className="w-8 h-8 bg-green-500 rounded-full"></div> {/* Reemplaza con tu icono */}
+          <h3 className="ml-2 text-lg font-bold">Título 2</h3>
+        </div>
+        <p>Texto descriptivo para el Recuadro 2.</p>
+      </div>
+
+      {/* Recuadro 3 */}
+      <div className="p-4 border border-gray-300 rounded-lg">
+        <div className="flex items-center mb-2">
+          <div className="w-8 h-8 bg-red-500 rounded-full"></div> {/* Reemplaza con tu icono */}
+          <h3 className="ml-2 text-lg font-bold">Título 3</h3>
+        </div>
+        <p>Texto descriptivo para el Recuadro 3.</p>
+      </div>
+
+      {/* Recuadro 4 */}
+      <div className="p-4 border border-gray-300 rounded-lg">
+        <div className="flex items-center mb-2">
+          <div className="w-8 h-8 bg-yellow-500 rounded-full"></div> {/* Reemplaza con tu icono */}
+          <h3 className="ml-2 text-lg font-bold">Título 4</h3>
+        </div>
+        <p>Texto descriptivo para el Recuadro 4.</p>
+      </div>
+      </section>
     </section>
   )
 }
