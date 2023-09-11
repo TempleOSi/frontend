872b9a73642585bc11c66310a1a6342a5db4bc4e
Max Benítez <plasmax115@Maxs-MacBook-Pro.local>
2023-09-10 23:12:17 -0600

    Form Frontend complete

--- components/form/HouseForm.tsx
+++ components/form/HouseForm.tsx
@@ -38,17 +38,23 @@ import { Slider } from "@/components/ui/slider"
 type SliderProps = React.ComponentProps<typeof Slider>
 
 const FormSchema = z.object({
-    street: z.string().min(1, {
-        message: "This field is required.",
-    }),
-    neigh: z.string().min(1, {
-        message: "This field is required.",
-    }),
-    postal: z.string().min(1, {
-        message: "This field is required.",
-    }),
-    price: z.number().int(),
-    lotarea: z.number().int()
+    // street: z.string().min(1, {
+    //     message: "This field is required.",
+    // }),
+    // neigh: z.string().min(1, {
+    //     message: "This field is required.",
+    // }),
+    // postal: z.string().min(1, {
+    //     message: "This field is required.",
+    // }),
+    // price: z.number().int(),
+    LotArea: z.number().int(),
+    GrLivArea: z.number().int(),
+    FullBath: z.any(),
+    HalfBath: z.any(),
+    KitchenAbvGr: z.any(),
+    GarageArea: z.number()
+
 })
 
 import { getTest } from "@/services/ml-services"
@@ -66,11 +72,16 @@ const HouseForm = () => {
     // }, []);
 
     const defaultValues = {
-        street: '',
-        neigh: '',
-        postal: '',
+        // street: '',
+        // neigh: '',
+        // postal: '',
         price: 50,
-        lotarea: 10000
+        LotArea: 10000,
+        GrLivArea: 10000,
+        FullBath: 1,
+        HalfBath: 1,
+        KitchenAbvGr: 1,
+        GarageArea: 10000
     };
 
     const form = useForm<z.infer<typeof FormSchema>>({
@@ -123,7 +134,7 @@ const HouseForm = () => {
 
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-                            <FormField
+                            {/* <FormField
                                 control={form.control}
                                 name="street"
                                 render={({ field }) => (
@@ -137,14 +148,14 @@ const HouseForm = () => {
                                         <FormMessage />
                                     </FormItem>
                                 )}
-                            />
+                            /> */}
                             <FormField
                                 control={form.control}
-                                name="lotarea"
+                                name="LotArea"
                                 render={({ field }) => (
                                     <FormItem className='mt-6'>
                                         <FormLabel className='mb-6'>
-                                            ¿Cuál es el tamaño de tu casa ideal?<br />
+                                            ¿Cuál es el tamaño de tu propiedad ideal? (LotArea)<br />
                                             {field.value} ft²
                                         </FormLabel>
                                         <FormControl>
@@ -165,20 +176,130 @@ const HouseForm = () => {
                             />
                             <FormField
                                 control={form.control}
-                                name="neigh"
+                                name="GrLivArea"
                                 render={({ field }) => (
-                                    <FormItem className="mt-6">
-                                        <FormLabel className="mt-10">Neighborhood</FormLabel>
+                                    <FormItem className='mt-6'>
+                                        <FormLabel className='mb-6'>
+                                            Por favor comparte el tamaño de la construcción habitable dentro de la propiedad (GrLivArea)<br />
+                                            {field.value} ft²
+                                        </FormLabel>
                                         <FormControl>
-                                            <Input className="mt-6" placeholder="Example: Glenn Dr. ... " {...field} />
+                                            <Slider
+                                                onValueChange={(newValue) => field.onChange(newValue[0])}
+                                                defaultValue={[10000]}
+                                                max={20000}
+                                                step={1}
+                                                className={cn("")}
+                                            />
                                         </FormControl>
                                         <FormDescription>
+
                                         </FormDescription>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                             />
                             <FormField
+                                control={form.control}
+                                name="FullBath"
+                                render={({ field }) => (
+                                    <FormItem className='mt-6'>
+                                        <FormLabel>¿Cuántos baños completos necesitas? (FullBath)</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Número de baños completos." />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value={1}>{1}</SelectItem>
+                                                <SelectItem value={2}>{2}</SelectItem>
+                                                <SelectItem value={3}>{3}</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormDescription>
+                                            Número de baños completos.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="HalfBath"
+                                render={({ field }) => (
+                                    <FormItem className='mt-6'>
+                                        <FormLabel>¿Cuántos medios baños necesitas? (HalfBath)</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Número de medios baños." />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value={1}>{1}</SelectItem>
+                                                <SelectItem value={2}>{2}</SelectItem>
+                                                <SelectItem value={3}>{3}</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormDescription>
+                                            Número de medios baños.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="KitchenAbvGr"
+                                render={({ field }) => (
+                                    <FormItem className='mt-6'>
+                                        <FormLabel>¿Cuántas cocinas necesitas? (KitchenAbvGr)</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Número de cocinas." />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value={1}>{1}</SelectItem>
+                                                <SelectItem value={2}>{2}</SelectItem>
+                                                <SelectItem value={3}>{3}</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormDescription>
+                                            Número de cocinas.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="GarageArea"
+                                render={({ field }) => (
+                                    <FormItem className='mt-6'>
+                                        <FormLabel className='mb-6'>
+                                            ¿Cuál es el tamaño de tu garage ideal? (GarageArea)<br />
+                                            {field.value} ft²
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Slider
+                                                onValueChange={(newValue) => field.onChange(newValue[0])}
+                                                defaultValue={[10000]}
+                                                max={20000}
+                                                step={1}
+                                                className={cn("")}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            {/* <FormField
                                 control={form.control}
                                 name="postal"
                                 render={({ field }) => (
@@ -202,29 +323,7 @@ const HouseForm = () => {
                                         <FormMessage />
                                     </FormItem>
                                 )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="price"
-                                render={({ field }) => (
-                                    <FormItem className='mt-6'>
-                                        <FormLabel className='mb-6'>Price: ${field.value}</FormLabel>
-                                        <FormControl>
-                                            <Slider
-                                                onValueChange={(newValue) => field.onChange(newValue[0])}
-                                                defaultValue={[50]}
-                                                max={100}
-                                                step={1}
-                                                className={cn("")}
-                                            />
-                                        </FormControl>
-                                        <FormDescription>
-
-                                        </FormDescription>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
+                            /> */}
                             <CardFooter>
                                 <Button className="w-full mt-6" type="submit">Submit</Button>
                             </CardFooter>
